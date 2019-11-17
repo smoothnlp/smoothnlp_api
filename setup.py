5d0a0058f1a2bb1abdebfa92c4d4a6f18712f3f9
zhangruinan <ruinan.zhang@icloud.com>
2019-11-17 21:45:32 +0800

    update version to 0.2.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ rootdir = os.path.abspath(os.path.dirname(__file__))
 long_description = open(os.path.join(rootdir, 'README.md')).read()
 
 NAME = "smoothnlp_api"
-VERSION = "0.1.5"
+VERSION = "0.2.0"
 # To install the library, run the following
 #
 # python setup.py install
